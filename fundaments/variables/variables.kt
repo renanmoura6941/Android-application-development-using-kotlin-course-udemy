b1e11141ec6f7f5bbadc29f3546c0704abe8867e
naneraruom <naneraruom@gmail.com>
2020-12-08 14:46:38 -0300

    practice variables

--- fundaments/variables/variables.kt
+++ fundaments/variables/variables.kt
@@ -1,3 +1,9 @@
 fun main(){
-
+    var generic = "generic";
+    var age: Int = 24; 
+    var name:String = "Renan";
+    var bankBalance: Double = 20.00 
+    println("My name is "+name+"\nage "+ age);
+    println("bank balance "+bankBalance);
+    println(generic);
 }
