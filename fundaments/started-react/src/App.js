485aa8d0ec5dc974ee257c04886ce42837433944
naneraruom <naneraruom@gmail.com>
2020-12-09 02:04:23 -0300

    primeiro componente

--- fundaments/started-react/src/App.js
+++ fundaments/started-react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+//formas  de criar componete react
+
+
+function AppFuncional(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>My name is Renan moura</h1>
   );
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <h1>what are you doing!</h1>
+    )
+  }
 }
 
 export default App;
